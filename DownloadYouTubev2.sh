de3923a0340ef6fc397465a0ed419506ad3ef928
awirthy <a.wirthy@gmail.com>
2023-01-27 15:06:59 +1100

    v0.14.4
    v0.14.4

--- DownloadYouTubev2.sh
+++ DownloadYouTubev2.sh
@@ -1,4 +1,4 @@
 # /etc/cron.d/ytdl
 # 
-pwsh /opt/DownloadYouTubev2/DownloadYouTubev2-latest/DownloadYouTubev2.ps1  >> /proc/1/fd/1;
+pwsh /opt/DownloadYouTubev2/DownloadYouTubev2-0.14.4/DownloadYouTubev2.ps1  >> /proc/1/fd/1;
 echo "test"  >> /proc/1/fd/1;
